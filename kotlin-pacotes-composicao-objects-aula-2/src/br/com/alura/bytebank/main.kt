9beefa8fd624022baee3d8e466267de0dbf374e0
2020nani <nani_almeida23@hotmail.com>
2021-06-23 14:15:53 -0300

    atualizando object]

--- kotlin-pacotes-composicao-objects-aula-2/src/br/com/alura/bytebank/main.kt
+++ kotlin-pacotes-composicao-objects-aula-2/src/br/com/alura/bytebank/main.kt
@@ -7,6 +7,10 @@ import br.com.alura.bytebank.modelo.Endereco
 
 
 fun main() {
+    var objectDeclaration = object {
+        var nome = "hernani"
+    }
+    println(objectDeclaration.nome)
     val endereco = Endereco(logradouro = "primeiro")
     val cliente = Cliente(nome = "Herm", cpf = "",endereco = endereco,senha = 123456)
     val clienteCC = ContaCorrente(titular = cliente,numero = 1)
